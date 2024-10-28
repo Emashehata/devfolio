bd859cb94a45398e2c3c150b7e6e5f7aea97fd56
ايمان عبدالفتاح ابوالحسن شحاته <ug_31107121@ics.tanta.edu.eg>
2024-10-28 14:08:42 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".auto-type",
+    {
+        strings : ["Freelancer","Designer","Devloper","Photographer"],
+        typeSpeed : 150 ,
+        backSpeed : 150 ,
+        loop :true
+    }
+) 
